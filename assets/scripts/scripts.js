640b80c7487414211e087aa9badb03f0f15f9b32
Cynthia Rojas Rivera <rori.cynthia@gmail.com>
2024-08-28 02:03:34 -0400

    Agrega nueva interactividad para tarjetas del proyecto y navbar.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,21 +1,34 @@
 function mouseOverHobbie() {
-        document.getElementById("dynamic-container").style.display = "block";
-        document.getElementById("hobbie-card-title").textContent = this.alt;
-        document.getElementById("hobbie-card-text").textContent = this.dataset.text;
+    document.getElementById("dynamic-container").style.display = "block";
+    document.getElementById("hobbie-card-title").textContent = this.alt;
+    document.getElementById("hobbie-card-text").textContent = this.dataset.text;
 }
 
 function mouseLeaveHobbie() {
     document.getElementById("dynamic-container").style.display = "none";
 }
 
-function mouseOverLinks () {
+function mouseOverLinks() {
     this.style.backgroundColor = "#FF4E88";
     this.style.color = "#FFFFFF";
 }
 
-function mouseLeaveLinks () {
+function mouseLeaveLinks() {
     this.style.backgroundColor = "#FEFEFE";
     this.style.color = "#FF4E88";
+}
+
+function mouseOverCards() {
+    this.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.3)";
+}
+
+function mouseLeaveCards() {
+    this.style.boxShadow = "none";
+}
+
+function onScrollNav() {
+    let scrolledNav = document.getElementById("navbar");
+    scrolledNav.style.boxShadow = "0 1px 8px rgba(0, 0, 0, 0.3)";
 }
 
 function loadEvents() {
@@ -25,9 +38,15 @@ function loadEvents() {
     };
 
     for (proyectLink of document.getElementsByClassName("project-card-link")) {
-        proyectLink.addEventListener("mouseover", mouseOverLinks)
+        proyectLink.addEventListener("mouseenter", mouseOverLinks)
         proyectLink.addEventListener("mouseleave", mouseLeaveLinks)
-    }
+    };
+
+    for (card of document.getElementsByClassName("project-card")) {
+        card.addEventListener("mouseenter", mouseOverCards)
+        card.addEventListener("mouseleave", mouseLeaveCards)
+    };
 }
 
 window.addEventListener("load", loadEvents);
+window.addEventListener("scroll", onScrollNav);
